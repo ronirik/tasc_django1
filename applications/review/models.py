99b2819500e47e3a414919b5f62161ee067972cf
ronirik <50165537+ronirik@users.noreply.github.com>
2022-02-21 00:40:06 +0600

    Update models.py

--- applications/review/models.py
+++ applications/review/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from applications.product.models import Product
 # from django.contrib.auth import get_user_model
-# from django.contrib.auth.models import User
+# User = get_user_model
+
 
 class Review(models.Model):
-    # User = get_user_model
-    # author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='review')
+    # author = models.ForeignKey(User, on_delete=models.CASCADE,)
     product = models.ForeignKey(Product,  on_delete=models.CASCADE, related_name='review')
     descr = models.TextField(max_length=250)
     rating = models.IntegerField(choices=list(zip(range(1, 6), range(1, 6))), unique=True)
     
 
     def __str__(self):
-        return f'{self.product} >>> {self.rating} >>>> {self.author}'
+        return f'{self.product} >>> {self.rating}'# >>>> {self.author}'
 
